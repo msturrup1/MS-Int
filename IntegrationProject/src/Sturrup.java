1baa0cd7cc4c3576a5452ee29ef548622cc37772
masturrup1387 <masturrup1387@H339R4S2.STUDENT.ad.fgcu.edu>
2017-09-21 09:41:33 -0400

    DO IT

---
+++ IntegrationProject/src/Sturrup.java
@@ -0,0 +1,17 @@
+
+public class Sturrup {
+
+	// A class is code
+	/**
+	 * Michael Sturrup
+	 * 
+	 * @param args
+	 */
+	
+	public static void main (String[] args) {
+		
+	System.out.println("Welcome to my Project!");
+		
+	}	
+	
+}
